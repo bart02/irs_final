46177f14f37c0ad9617a38393c77d0b0e5f7ddc3
ViktorDanilin <danilinvs58@gmail.com>
2022-03-21 14:18:45 +0300

    main

--- main.py
+++ main.py
@@ -1,7 +1,27 @@
+import time
+from math import pi
+from OperateCamera import OperateCamera
+from UR10E import UR10E
+
+
+robot = UR10E()
+cam = OperateCamera()
+data = "data_set/test0.ply"
+
+
+def catch_img():
+    frame = cam.catch_frame()
+    cam.save(data)
+    pcd = cam.open(data)
+    cam.visualization_of_points(pcd)
+    return frame
 
 def main():
-    pass
+    robot.initPos()
+    robot.close()
+    frame = catch_img()
+    print(frame.points[0])
+    print(frame.colors[0])
 
 if __name__ == "__main__":
-
     main()
