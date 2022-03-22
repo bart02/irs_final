a17f2f4d0431a28ff6e9af8024f3e8e4c782d0bc
ViktorDanilin <danilinvs58@gmail.com>
2022-03-23 00:32:32 +0300

    final nice dick, awesome balls (very work)

--- main.py
+++ main.py
@@ -1,32 +1,56 @@
+from libs.Camera import Camera, DummyCamera
+from libs.Detail import Detail
 from libs.UR10E import UR10E
 
-#robot = UR10E("localhost")
-robot = UR10E("172.31.1.25")
+# robot initialization
+try: robot = UR10E('localhost')
+except ConnectionError: robot = UR10E('172.31.1.25')
+camera = DummyCamera(fn='data_set/1647866163.3144376.jpg')
 
+ZONE = {'blue': [-0.89409, 0.26178, 0.33163],
+        'red': [-0.705, 0.260930, 0.332240]}
+HEIGHT = 0.26
 
-# docker
-# run - d - -name = "dockursim" - e
-# ROBOT_MODEL = UR10 - p
-# 8080: 8080
-# -p
-# 29999: 29999 - p
-# 30001 - 30004: 30001 - 30004 - v / mnt / c / Users / Viktor / PycharmProjects / irs_final: / ursim / programs - v
-# dockursim: / ursim - -privileged - -cpus = 1 - -gpus = all
-# arranhs / dockursim: latest
 def main():
+    towers: dict[str, list[Detail]] = {'blue': [], 'red': []}
+    cur = 'blue'
+    while True:
+        # move to init state to take picture
+        robot.initTool()
 
-    robot.open_gripper()
-    # get z off-set pose
-    x = -0.133
-    y = -0.259
-    z = 0 #z off-set
-    #robot.setPos(x, y, z, 0)
-    robot.close_gripper()
+        # detect cube
+        frame = camera.take_photo()
+        details = frame.find_str_details(cur)
+        if len(details) == 0:
+            break
+        detail = details[0]
+        print(detail)
 
-    # end main
+        # pick cube
+        robot.open_gripper()
+        robot.rotateTool(-100, detail.angle)
+        robot.moveTool(detail.center_m)
+        robot.moveTool([0])
+        robot.close_gripper()
+
+        # go upper and rotate
+        robot.moveTool([0.1])
+        robot.rotateTool(None, 0)
+
+        # place cube to zone
+        z = ZONE[cur].copy()
+        z[2] += len(towers[cur]) * HEIGHT
+        print(z)
+        robot.moveTool(z[0:2], True)
+        robot.moveTool([z[2]], True)
+        robot.open_gripper()
+        robot.moveTool([0.1])
+
+        # switch zone
+        cur = 'blue' if cur == 'red' else 'red'
+
     robot.close()
 
 
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
