c3f6a16445019849b3adb3576426422119446d68
Artem Batalov <a@batalov.me>
2022-03-22 23:29:47 +0300

    New Camera functions

--- main_with_cv.py
+++ main_with_cv.py
@@ -1,50 +1,54 @@
-import cv2
-
-# connect to robot
-from libs.CVProcessImage import CVProcessImage
+from libs.Camera import Camera
+from libs.Detail import Detail
 from libs.UR10E import UR10E
 
 # robot initialization
 try: robot = UR10E("localhost")
 except ConnectionError: robot = UR10E("172.31.1.25")
-cam = cv2.VideoCapture(0)
+
+camera = Camera()
 
 ZONE = {'blue': [-0.89409, 0.26178, 0.33163],
         'red': [-0.705, 0.260930, 0.332240]}
+HEIGHT = 0.26
 
 def main():
-    height = {'blue': 0, 'red': 0}
+    towers: dict[str, list[Detail]] = {'blue': [], 'red': []}
     cur = 'blue'
     while True:
         # move to init state to take picture
         robot.initTool()
 
         # detect cube
-        ret, frame = cam.read()
-        im = CVProcessImage(fn='data_set/img.png')
-        rects = im.get_rects(im.blue_thresh if cur == 'blue' else im.red_thresh)
-        if len(rects) == 0: break
+        frame = camera.take_photo()
+        details = frame.find_str_details(cur)
+        if len(details) == 0:
+            break
+        detail = details[0]
 
         # pick cube
         robot.open_gripper()
-        robot.rotateTool(-100, rects[0][1])
-        robot.moveTool(rects[0][0][0], rects[0][0][1], 0)
+        robot.rotateTool(-100, detail.angle)
+        robot.moveTool(*detail.center_m)
         robot.close_gripper()
 
-        # place cube to zone
+        # go upper and rotate
         robot.moveTool(0.1)
         robot.rotateTool(None, 0)
-        z = ZONE[cur]
-        z[2] += height[cur]
-        height[cur] += 0.026
-        robot.moveTool(z[0], z[1], z[2])
+
+        # place cube to zone
+        z = ZONE[cur].copy()
+        z[2] += len(towers[cur]) * HEIGHT
+
+        robot.moveTool(z)
         robot.open_gripper()
         robot.moveTool(0.1)
 
         # switch zone
-        cur = 'blue' if cur == 'red' else 'blue'
+        cur = 'blue' if cur == 'red' else 'red'
 
     robot.close()
+
 
 if __name__ == "__main__":
     main()
