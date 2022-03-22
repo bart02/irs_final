4a597197b3ac1a6905df9919cdeb63516f5a0dce
Artem Batalov <a@batalov.me>
2022-03-22 14:02:21 +0300

    main with cv

---
+++ main_with_cv.py
@@ -0,0 +1,42 @@
+import cv2
+
+from libs.CVProcessImage import CVProcessImage
+from libs.UR10E import UR10E
+
+#robot = UR10E("localhost")
+robot = UR10E("172.31.1.25")
+
+cam = cv2.VideoCapture(0)
+
+
+# docker
+# run - d - -name = "dockursim" - e
+# ROBOT_MODEL = UR10 - p
+# 8080: 8080
+# -p
+# 29999: 29999 - p
+# 30001 - 30004: 30001 - 30004 - v / mnt / c / Users / Viktor / PycharmProjects / irs_final: / ursim / programs - v
+# dockursim: / ursim - -privileged - -cpus = 1 - -gpus = all
+# arranhs / dockursim: latest
+def main():
+    robot.open_gripper()
+
+    ret, frame = cam.read()
+    im = CVProcessImage(frame=frame)
+    rects = im.get_rects(im.blue_thresh)
+
+    # get z off-set pose
+    x = rects[0][0][0]
+    y = rects[0][0][1]
+    z = 0 #z off-set
+    print(x,y)
+    robot.setPos(x, y, z, 0)
+    robot.close_gripper()
+
+    # end main
+    robot.close()
+
+
+
+if __name__ == "__main__":
+    main()
