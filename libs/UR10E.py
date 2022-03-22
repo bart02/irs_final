c39cc176383a2192de7053e8ad407b26cf6b65d7
ViktorDanilin <danilinvs58@gmail.com>
2022-03-22 12:01:23 +0300

    2sd dey

--- libs/UR10E.py
+++ libs/UR10E.py
@@ -11,15 +11,15 @@ class UR10E(OperateRobot):
         if self.startPos['z'] - dz < 0.07:
             raise Exception('you are dumb')
         t = self.getl()
-        offset = [0, 0, 362.5/1000]
-        rotation = 0 # calculate with angle
+        offset = [-70/1000, 10/1000, -362.5/1000]
         t[0] = self.startPos['x'] + dx + offset[0]
         t[1] = self.startPos['y'] + dy + offset[1]
         t[2] = self.startPos['z'] - dz + offset[2]
-        t[3] = self.startPos['rx'] + rotation
-        t[4] = self.startPos['ry'] + rotation
-        t[5] = 0
+        t[3] = 3.015
+        t[4] = -0.906
+        t[5] = ang
         self.movel_list(t)
 
     def initPos(self):
         self.movel(self.startPos)
+
